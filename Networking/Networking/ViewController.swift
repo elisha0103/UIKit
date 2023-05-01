0f049123c2743962358fc14f2015db43dd9ed05d
Taeyoung Jin <elisha0103@naver.com>
2023-05-01 20:25:25 +0900

    URLSession과 View 업데이트의 스레드 조절

--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -15,11 +15,26 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         cell.textLabel?.text = friend.name.full
         cell.detailTextLabel?.text = friend.email
+        cell.imageView?.image = nil
         
-        guard let imageURL: URL = URL(string: friend.picture.thumbnail) else { return cell }
-        guard let imageData: Data = try? Data(contentsOf: imageURL) else { return cell }
-        
-        cell.imageView?.image = UIImage(data: imageData)
+        // 백그라운드 스레드에서 다운로드 하도록
+        DispatchQueue.global().async {
+            guard let imageURL: URL = URL(string: friend.picture.thumbnail) else { return }
+            guard let imageData: Data = try? Data(contentsOf: imageURL) else { return }
+
+            // 다운로드한 이미지를 뷰에 적용하도록
+            DispatchQueue.main.async {
+                // 다운로드 중 셀의 인덱스 정보가 바뀔 수 있다.(스크롤의 여부로)
+                if let index: IndexPath = tableView.indexPath(for: cell) {
+                    // index 변수: 다운로드하려는 cell의 인덱스 정보
+                    // indexPath: 현재 나타난 새로운 cell의 인덱스 정보
+                    if index.row == indexPath.row {
+                        cell.imageView?.image = UIImage(data: imageData)
+
+                    }
+                }
+            }
+        }
         
         return cell
     }
@@ -54,7 +69,10 @@ class ViewController: UIViewController, UITableViewDataSource {
             do {
                 let apiResponse: APIResponse = try JSONDecoder().decode(APIResponse.self, from: data)
                 self.friends = apiResponse.results
-                self.tableView.reloadData()
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             } catch(let err) {
                 print(err.localizedDescription)
             }
