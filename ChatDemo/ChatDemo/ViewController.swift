4e64018489c61b1513358e8acddbf2c88e4e1d69
Taeyoung Jin <elisha0103@naver.com>
2023-10-27 14:57:12 +0900

    [init]: ChatDemoProject
    - ChatDemoProject initializer

---
+++ ChatDemo/ChatDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatDemo
+//
+//  Created by 진태영 on 10/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
